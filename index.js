5cd8a4acb3e48f92cced16c5c00a9201f1645037
Daniel Mullaney <danftv@gmail.com>
2021-01-11 00:28:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ function happyHolidays () {
 function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`
 }
-function happyCustomHolidayTo (holiday + name) {
-  return `Happy ${holiday}, ${name}!`
-}
+
